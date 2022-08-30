dda19f26689dbdf89126c931c19a07396efaee5f
rtheodoro <rtheodoro@usp.br>
2022-08-30 16:40:07 -0300

    readme atualizado

---
+++ Codigo/1_importacao_e_tratamento.R
@@ -0,0 +1,114 @@
+# -------------------------------------------------------------------------#
+#                   Importando e tratando base
+#
+#  Script criado para importar e tratar os dados da base
+#  SurvAlun - EstProb I.xlsx
+#
+# -------------------------------------------------------------------------#
+
+
+# Pacotes utilizados ------------------------------------------------------
+
+# Instalar antes:
+install.packages(c("dplyr", "janitor", "lubridate", "readxl", "stringr"))
+
+library(dplyr)
+library(janitor)
+library(lubridate)
+library(readxl)
+library(stringr)
+
+# Importando --------------------------------------------------------------
+
+survAluno <- readxl::read_xlsx("Aula-RCC0219/data_input/SurvAlun - EstProb I.xlsx")
+
+# Resumo
+dplyr::glimpse(survAluno)
+
+# Tratando ----------------------------------------------------------------
+
+## Limpando nomes ----
+# Se os nomes fossem curtos, poderiamos usar a função
+# janitor::clean_names(), mas nesse caso precisamos reduzir o tamanho
+
+names(survAluno) <- c(
+  "data_hora", "altura", "n_calcado", "peso", "cirurgia", "estetica",
+  "estetica_outros", "cor", "oculos", "sexo", "estado_civil",
+  "n_pessoas_moradia", "entidade_estudantil", "tipo_entidade_estudantil",
+  "religiao", "animais_domesticos", "outro_animais_domesticos", "n_filhos",
+  "idade", "doa_sangue", "frequencia_esporte", "n_livros_ano", "uf_antes_fearp",
+  "n_pessoas_whatsapp", "renda_em_sm", "auxilio_usp", "tipo_auxilio_usp",
+  "bandejao", "n_refeicoes_bandejao", "fundo_de_investimento", "cartao_de_credito",
+  "conta_em_bancos", "previdencia_privada", "ano_ingresso_usp",
+  "cotas", "divide_residencia", "n_tv", "viagem_exterior", "n_paises",
+  "idioma_fluente", "outro_idioma", "plano_saude", "escola_colegial",
+  "casa_pais", "escolaridade_pai", "escolaridade_mae", "ajuda_financeira_pais",
+  "transporte_em_ribeirao", "graduacao_anterior", "curso_graduacao_anterior",
+  "empregado", "modalidade_emprego", "profissao", "trabalho_contabil",
+  "cidade_empresa", "horas_trabalho_semana", "renda_complementar"
+)
+
+dplyr::glimpse(survAluno)
+
+str(survAluno)
+
+# Separando colunas
+survAluno <- survAluno |>
+  dplyr::mutate(
+    data = lubridate::ymd(as.Date(data_hora)),
+    hora = stringr::str_sub(data_hora, start = 12),
+    hora = lubridate::hms(hora)
+  ) |>
+  dplyr::select(data_hora, data, hora, everything())
+
+max(survAluno$data_hora) - min(survAluno$data_hora)
+max(survAluno$data) - min(survAluno$data)
+max(lubridate::hour(survAluno$hora)) - min(lubridate::hour(survAluno$hora))
+
+## Resumo de respostas ----
+
+# Estatística descritiva das variáveis numéricas
+library(tidyselect)
+survAluno |>
+  dplyr::select(where(is.double)) |>
+  summary()
+
+# Resumo das respostas categóricas
+survAluno |>
+  dplyr::group_by(animais_domesticos) |>
+  dplyr::count() |>
+  dplyr::arrange(n) |>
+  janitor::adorn_totals()
+
+
+### Criando função ----
+resumo_categorica <- function(base, variavel) {
+  base |>
+    dplyr::group_by({{ variavel }}) |>
+    dplyr::count() |>
+    dplyr::arrange(n) |>
+    janitor::adorn_totals()
+}
+
+# Criando uma iteração para todas as colunsa de vuma vez
+for (i in 1:ncol(survAluno)) {
+   resumo_categorica(survAluno, survAluno[, i]) |>
+    print()
+}
+
+
+# Filtrando  ----
+
+survAluno |>
+   dplyr::filter(trabalho_contabil == "Sim") |>
+   dplyr::group_by(horas_trabalho_semana) |>
+   dplyr::count() |>
+   janitor::adorn_totals()
+
+# Agrupando ----
+
+survAluno |>
+   dplyr::group_by(trabalho_contabil, horas_trabalho_semana) |>
+   dplyr::count()
+
+
--- README.md
+++ README.md
@@ -10,11 +10,13 @@ O objetivo deste projeto é ensinar o básico da programação em R para a turma
 | Introdução               |  [1_slides_introducao.pdf](https://github.com/rtheodoro/RCC0219-ProbEstAplCont/blob/main/Slides/2_slides_rrstudio.pdf)    |  - |
 | R e RStudio              |  [2_slide_rrstudio.pdf](https://github.com/rtheodoro/RCC0219-ProbEstAplCont/blob/main/Slides/2_slides_rrstudio.pdf)     |  [2_codigos_rrstudio](https://github.com/rtheodoro/RCC0219-ProbEstAplCont/blob/main/Codigo/2_codigos_rrstudio.r) |
 | Projetos, objetos, funções e pacotes |  [3_slides_projetos_objetos_funcoes_pacotes.pdf](https://github.com/rtheodoro/RCC0219-ProbEstAplCont/blob/main/Slides/3_slides_projetos_objetos_funcoes_pacotes.pdf)     |  [3_codigos_projetos_objetos_funcoes_pacotes](https://github.com/rtheodoro/RCC0219-ProbEstAplCont/blob/main/Codigo/3_codigos_projetos_objetos_funcoes_pacotes.R) |
-| Importação e Tratamento de Dados |  4_slide     |  4_codigo |
+| Importação e Tratamento de Dados |  [4_importacao_e_tratamento.pdf](https://github.com/rtheodoro/RCC0219-ProbEstAplCont/blob/main/Slides/4_slides_importacao_e_tratamento.pdf)     |  [4_importacao_e_tratamento.pdf](https://github.com/rtheodoro/RCC0219-ProbEstAplCont/blob/main/Codigo/4_importacao_e_tratamento.R) |
 | Estatísticas e Visualização      |  5_slide     |  5_codigo | 
 
 Os Slides originais estão na pasta `Slides` e com a extenção `.html`.
 Os Códigos estão na pasta `Codigos`.
+
+Na pasta `Aula-RCC0219` estão os arquivos utilizados na aula prática, inclusive exercícios de treino.
 
 ## Referências
 
