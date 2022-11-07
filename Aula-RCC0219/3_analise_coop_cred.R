fa41fcb4cae28f39dd80f6abb1b4b90d3fdc2149
rtheodoro <rtheodoro@usp.br>
2022-11-07 11:56:52 -0300

    alterações

--- Aula-RCC0219/3_analise_coop_cred.R
+++ Aula-RCC0219/3_analise_coop_cred.R
@@ -16,7 +16,9 @@
 # Notas: Este script contém o que está no arquivo analise_auditores_coopcred.Rmd
 #
 #--------------------------------------------------------------------------#
-options(scipen = 6, digits = 2)
+options(scipen = 6, digits = 2) 
+# transforma notação científica em n inteiro
+# arredondo duas cadas decimais 
 #--------------------------------------------------------------------------#
 
 
@@ -38,7 +40,6 @@ coop_cred_2021_auditores <-
 
 
 # Resumo
-
 dplyr::glimpse(coop_cred_2021_auditores)
 
 
@@ -59,13 +60,13 @@ coop_cred_2021_auditores |>
   dplyr::select(where(is.numeric)) |>
   summary()
 
-# Os números estão sem as casas decimais
+# Os números estão muito grandes, fica difívil visualiar
 
 coop_cred_2021_auditores <-
-  coop_cred_2021_auditores |> dplyr::mutate(dplyr::across(
-    where(is.numeric) &
-      !c(cnpj, big_four, numero_agencias),
-    .fns = ~ . / 100
+  coop_cred_2021_auditores |>
+  dplyr::mutate(dplyr::across(
+    where(is.numeric) & !c(cnpj, big_four, numero_agencias),
+    .fns = ~ . / 100000000
   ))
 
 # Estatísticas descritivas
@@ -122,6 +123,74 @@ coop_cred_2021_auditores |>
   print(n = Inf )
 
 
+coop_cred_2021_auditores |>
+  dplyr::filter(uf == "SP" & big_four == 1) |>
+  dplyr::group_by(uf) |>
+  dplyr::count()
+
+
+## Boxplot - visualização dos quartis ------------------------------------
+
+boxplot(
+  coop_cred_2021_auditores$ativo_total,
+  main = "Boxplot do Ativo Total",
+  ylab = "Ativo Total",
+  xlab = "Distribuição",
+  col = "darkgreen"
+)
+
+quantile(coop_cred_2021_auditores$ativo_total)
+
+boxplot(
+  coop_cred_2021_auditores$patrimonio_liquido,
+  main = "Boxplot do Patrimônio Líquido",
+  ylab = "Patrimônio Líquido",
+  xlab = "Distribuição",
+  col = "darkgreen"
+)
+
+
+quantile(coop_cred_2021_auditores$patrimonio_liquido)
+
+
+# Removendo outliers
+
+coop_cred_2021_auditores <- coop_cred_2021_auditores |>
+  dplyr::filter(ativo_total <= 1000 &
+                  patrimonio_liquido <= 100)
+
+
+
+coop_cred_2021_auditores |>
+  janitor::tabyl(big_four)
+
+
+boxplot(
+  coop_cred_2021_auditores$ativo_total,
+  main = "Boxplot do Ativo Total",
+  ylab = "Ativo Total",
+  xlab = "Distribuição",
+  col = "darkgreen"
+)
+
+
+boxplot(
+  coop_cred_2021_auditores$patrimonio_liquido,
+  main = "Boxplot do Patrimônio Líquido",
+  ylab = "Patrimônio Líquido",
+  xlab = "Distribuição",
+  col = "darkgreen"
+)
+
+
+boxplot(ativo_total ~ big_four, 
+        coop_cred_2021_auditores,
+        main = "Boxplot do Ativo Total Por Grupos",
+        ylab = "Ativo Total",
+        xlab = "Big Four",
+        col = "darkgreen")
+
+
 ## Histograma - gráfico de densidade/frequência ---------------------------
 
 hist(
@@ -146,77 +215,99 @@ hist(
 # Teste de normalidade
 shapiro.test(coop_cred_2021_auditores$patrimonio_liquido) # p-valor < 0,05 -> A distribuição não é normal
 
-## Boxplot - visualização dos quartis ------------------------------------
 
-boxplot(
-  coop_cred_2021_auditores$ativo_total,
-  main = "Boxplot do Ativo Total",
-  ylab = "Ativo Total",
-  xlab = "Distribuição",
-  col = "darkgreen"
+# Normalizando variáveis
+coop_cred_2021_auditores <- coop_cred_2021_auditores |>
+  dplyr::mutate(ativo_log = log(ativo_total),
+                pl_log = log(patrimonio_liquido))
+
+
+hist(
+  coop_cred_2021_auditores$ativo_log,
+  freq = FALSE,
+  main = "Distribuição do log(Ativo Total)",
+  ylab = "Densidade",
+  xlab = "Valores do log(Ativo Total)"
 )
 
+# Teste de normalidade
+shapiro.test(coop_cred_2021_auditores$ativo_log) # p-valor < 0,05 -> A distribuição não é normal, mas se aproxima
 
-boxplot(
-  coop_cred_2021_auditores$patrimonio_liquido,
-  main = "Boxplot do Patrimônio Líquido",
-  ylab = "Patrimônio Líquido",
-  xlab = "Distribuição",
-  col = "darkgreen"
+hist(
+  coop_cred_2021_auditores$pl_log,
+  freq = FALSE,
+  main = "Distribuição do log(Patrimônio Líquido)",
+  ylab = "Densidade",
+  xlab = "Valores do log(Patrimônio Líquido)"
 )
 
-
-# Removendo outliers
-
-!ativo_total_1m %in% boxplot.stats(ativo_total)$out
-
+# Teste de normalidade
+shapiro.test(coop_cred_2021_auditores$pl_log) # p-valor < 0,05 -> A distribuição não é normal, mas se aproxima
 
 # Associação entre variáveis qualitativas (chi2 e Pearson) ----------
 
-chisq_filiacao <- chisq.test(coop_cred_2021_auditores$big_four,
-                             coop_cred_2021_auditores$filiacao)
+chisq.test(coop_cred_2021_auditores$big_four,
+           coop_cred_2021_auditores$filiacao)
 
-print(chisq_filiacao)
 
-chisq_uf <- chisq.test(coop_cred_2021_auditores$big_four,
-                       coop_cred_2021_auditores$uf)
+chisq.test(coop_cred_2021_auditores$big_four,
+           coop_cred_2021_auditores$uf)
 
-print(chisq_uf)
-
+# p-valor < 0,05 grupos iguais
 
 # Assocoação entre variáveis quantitativas (covariância e correlaç --------
 
+chisq.test(coop_cred_2021_auditores$big_four,
+           coop_cred_2021_auditores$pl_log)
 
 chisq.test(coop_cred_2021_auditores$big_four,
-           coop_cred_2021_auditores$patrimonio_liquido)
+           coop_cred_2021_auditores$ativo_log)
 
-chisq.test(coop_cred_2021_auditores$big_four,
-           coop_cred_2021_auditores$ativo_total)
+# p-valor = 0,05, grupos diferentes
 
 # Correlação
 cor(
-  coop_cred_2021_auditores |> dplyr::filter(big_four == 0) |> dplyr::select(patrimonio_liquido),
-  coop_cred_2021_auditores |> dplyr::filter(big_four == 0) |> dplyr::select(ativo_total),
+  coop_cred_2021_auditores |> dplyr::filter(big_four == 0) |> dplyr::select(pl_log),
+  coop_cred_2021_auditores |> dplyr::filter(big_four == 0) |> dplyr::select(ativo_log),
   method = "pearson"
 )
 
 cor(
-  coop_cred_2021_auditores |> dplyr::filter(big_four == 1) |> dplyr::select(patrimonio_liquido),
-  coop_cred_2021_auditores |> dplyr::filter(big_four == 1) |> dplyr::select(ativo_total),
+  coop_cred_2021_auditores |> dplyr::filter(big_four == 1) |> dplyr::select(pl_log),
+  coop_cred_2021_auditores |> dplyr::filter(big_four == 1) |> dplyr::select(ativo_log),
   method = "pearson"
 )
+
+# Próximo de 1, são autamente correlacionadas
+
 
 # Covariância
 
 cov(
-  coop_cred_2021_auditores$patrimonio_liquido,
-  coop_cred_2021_auditores$ativo_total
+  coop_cred_2021_auditores$pl_log,
+  coop_cred_2021_auditores$ativo_log
 )
+
+# Auta covariância
 
 
 # Associação entre variáveis qualitativas e quantitativas (R^2) --------
 
 r2 <-
-  lm(patrimonio_liquido ~ big_four, coop_cred_2021_auditores) |> summary()
+  lm(pl_log ~ ativo_log + big_four, coop_cred_2021_auditores) |> summary()
 
-r2$r.squared # quanto maior, melhor
+r2$r.squared # quanto maior, melhor.
+
+
+# Gráficos ----------------------------------------------------------------
+
+coop_cred_2021_auditores |>
+  ggplot2::ggplot() +
+  ggplot2::aes(x = pl_log, y = ativo_log) +
+  ggplot2::geom_point(colour = "#011e5a") +
+  ggplot2::xlab("Patrimônio Líquido") +
+  ggplot2::ylab("Ativo Total") +
+  ggplot2::labs(title = "Relação Patrimônio Líquido X Ativo Total") +
+  ggplot2::geom_smooth(method = "lm")
+
+
